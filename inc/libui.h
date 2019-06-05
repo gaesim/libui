7e25ccf0d407c7d64c643a4e6fcb4a1db74fcf3b
Alicia BERRAHMA <aberrahm@student.42.fr>
2019-06-05 21:18:33 +0200

    work in progress

--- inc/libui.h
+++ inc/libui.h
@@ -6,7 +6,7 @@
 /*   By: aberrahm <aberrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 01:02:30 by gsimeon           #+#    #+#             */
-/*   Updated: 2019/06/05 18:09:49 by aberrahm         ###   ########.fr       */
+/*   Updated: 2019/06/05 21:18:09 by aberrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int		ui_init(void);
 /*
 **		error
 */
-
+t_sdl   create_window(const char *title, flags xy, t_coord wh, flags win);
 int     ui_error(Uint32 error, const char *file, const char *func, const int line);
 int		ui_exit(Uint32 error);
 
--- src/init/ui_window.c
+++ src/init/ui_window.c
@@ -6,7 +6,21 @@
 /*   By: aberrahm <aberrahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 18:06:43 by aberrahm          #+#    #+#             */
-/*   Updated: 2019/06/05 18:06:46 by aberrahm         ###   ########.fr       */
+/*   Updated: 2019/06/05 21:17:51 by aberrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libui.h"
+
+// besoin de l initialisation pour creer la fenetre mais fonction doit etre independante.
+// pour les flags de la fonction voir sdl_video.h
+
+t_sdl   create_window(const char *title, flags xy, t_coord wh, flags win)
+{
+    t_sdl   *win;
+    
+    win = SDL_Create_Window(&title, )
+
+
+    return (&win);
+}
