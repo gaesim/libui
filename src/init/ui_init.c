60f43b4ea83c715d030b757a6fae1d56e6f064d2
Gaetan SIMEON <gsimeon@student.42.fr>
2019-07-15 22:57:57 +0200

    [FIX] remove test case of return 0 on init by SDL

--- src/init/ui_init.c
+++ src/init/ui_init.c
@@ -6,7 +6,7 @@
 /*   By: gsimeon <gsimeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 01:22:35 by gsimeon           #+#    #+#             */
-/*   Updated: 2019/04/07 18:56:20 by gsimeon          ###   ########.fr       */
+/*   Updated: 2019/07/15 22:57:24 by gsimeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int ui_init(void)
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 		return(ui_error(1, __FILE__, __func__, __LINE__));
 	return (0);
 }
